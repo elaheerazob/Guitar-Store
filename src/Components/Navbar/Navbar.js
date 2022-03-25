122395a13c37ccea11508fedb7bac89ab380e601
elaheerazob <ridwanelahee282@gmail.com>
2022-03-25 22:16:12 +0600

    add all file

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <h1>Guitar Store</h1>
+      
+    </nav>
+  );
+};
+
+export default Navbar;
